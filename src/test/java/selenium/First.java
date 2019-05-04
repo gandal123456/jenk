f7a6e126c31d8f7310fbb61cc969d4655ae257a8
“Ganesh123456” <ganeshgandal@gmail.com>
2019-05-04 11:12:41 +0530

    First time

---
+++ src/test/java/selenium/First.java
@@ -0,0 +1,27 @@
+package selenium;
+
+
+
+import org.testng.annotations.Test;
+
+public class First 
+{
+	@Test
+	public void one()
+	{
+	System.out.println("One runs well");
+	}
+	
+	@Test
+	public void two()
+	{
+	System.out.println("two runs well");
+	}
+	
+	@Test
+	public void three()
+	{
+	System.out.println("three runs well");
+	}
+
+}
---
+++ src/test/java/selenium/Second.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class Second
+{
+
+	@Test
+	public void four()
+	{
+	System.out.println("four runs well");
+	}
+	
+	@Test
+	public void five()
+	{
+	System.out.println("five runs well");
+	}
+	
+	@Test
+	public void six()
+	{
+	System.out.println("six runs well");
+	}
+}
